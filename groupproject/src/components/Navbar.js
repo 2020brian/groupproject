02dc59be1d8338b100c8bd4368dbc5ebc699850d
Ochuodho <ochuodhoderrick@gmail.com>
2023-08-30 22:23:40 +0300

    feat: authentication

--- groupproject/src/components/Navbar.js
+++ groupproject/src/components/Navbar.js
@@ -2,9 +2,10 @@
 import { NavLink } from "react-router-dom"; 
 import { BsFillHouseHeartFill } from 'react-icons/bs'
 import { FaTruckMoving } from 'react-icons/fa';
-import { FaShoppingBag } from 'react-icons/fa'
+import { FaShoppingBag } from 'react-icons/fa';
 import { LuLogIn } from 'react-icons/lu'
 import { LuLogOut } from 'react-icons/lu'
+import { useAuth0 } from "@auth0/auth0-react";
 
 import { FaUser } from 'react-icons/fa'
 import './Navbar.css'
@@ -12,11 +13,13 @@ import './Navbar.css'
 
 
 function Navbar() {
+  const { loginWithRedirect, logout, user, isAuthenticated } = useAuth0();
   return (
     <>
     <div className="free">
      
         <div className='icons'>
+          
         <FaTruckMoving /> 
         </div>
         <p>Free shipping upto Kshs 1000!!!  </p>
@@ -31,17 +34,28 @@ function Navbar() {
             <button>Search</button>
           </div>
           <div className='icon'>
-              <div className='account'>
+            {
+            
+              isAuthenticated &&
+              (
+                <div className='account'>
                 <div className='iconuser'>
                 <FaUser />
+                
+                <p>Hello, {user.name}</p>
                 </div>
-                <p>Hello, user</p>
+                </div>
+
+              )
+            }
+            
+              
                 <div className='icon2'>
                   <NavLink to="/" className='routelink'>Home<BsFillHouseHeartFill /></NavLink>
                   <NavLink to="/cart" className='routelink'>Cart<FaShoppingBag /></NavLink>
                 </div>
               </div>
-            </div>
+           
           </div>
 
         </div>
@@ -71,8 +85,14 @@ function Navbar() {
             
           </div>
           <div className='authentication-container'>
-            <LuLogIn />
-            <LuLogOut />
+            {
+              isAuthenticated ? 
+              <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>Logout<LuLogOut /></button>
+              :
+              <button onClick={() => loginWithRedirect()}>Login<LuLogIn /></button>
+            }
+            
+            
           </div>
             
           </div>
