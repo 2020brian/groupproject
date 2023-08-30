734c9a0237f5875214f198f7fba5f9a69eafd4dc
Ochuodho <ochuodhoderrick@gmail.com>
2023-08-31 00:26:56 +0300

    feat: styling the homepage

--- groupproject/src/components/Home.js
+++ groupproject/src/components/Home.js
@@ -1,12 +1,13 @@
 import {NavLink} from 'react-router-dom'
+import './home.css'
 export default function Home(){
     return (
         <>
-        <div className='top-banner'>
+        <div className='top'>
             <div className='container'>
                 <div className='detail'>
                     <h2>The best online store</h2>
-                    <NavLink to='/product'>Shop Now</NavLink>
+                    <NavLink to='/product' className='productlink'>Shop Now</NavLink>
                 </div>
                 <div className="image-box">
                     <img src='./images/img/sliding-image.png' alt="shopify" ></img>
