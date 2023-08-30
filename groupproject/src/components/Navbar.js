285d5eb02ce29a5b724a895d5c2be8808c928ad9
Ochuodho <ochuodhoderrick@gmail.com>
2023-08-30 19:31:45 +0300

    feat: styling the navbar

--- groupproject/src/components/Navbar.js
+++ groupproject/src/components/Navbar.js
@@ -3,6 +3,9 @@ import { NavLink } from "react-router-dom";
 import { BsFillHouseHeartFill } from 'react-icons/bs'
 import { FaTruckMoving } from 'react-icons/fa';
 import { FaShoppingBag } from 'react-icons/fa'
+import { LuLogIn } from 'react-icons/lu'
+import { LuLogOut } from 'react-icons/lu'
+
 import { FaUser } from 'react-icons/fa'
 import './Navbar.css'
 
@@ -26,32 +29,57 @@ function Navbar() {
           <div className='search'>
             <input type='text' value='' placeholder='search product'></input>
             <button>Search</button>
-            <div className='icon'>
+          </div>
+          <div className='icon'>
               <div className='account'>
                 <div className='iconuser'>
                 <FaUser />
                 </div>
                 <p>Hello, user</p>
-              
-
+                <div className='icon2'>
+                  <NavLink to="/" className='routelink'>Home<BsFillHouseHeartFill /></NavLink>
+                  <NavLink to="/cart" className='routelink'>Cart<FaShoppingBag /></NavLink>
+                </div>
               </div>
-              <BsFillHouseHeartFill />
-              <FaShoppingBag />
-
             </div>
-
           </div>
-        </div>
-
-
-
 
         </div>
-     
-    
-      
-    
-  
+        <div className='header-container'>
+          
+          <div className='container'>
+            <div className='menu'>
+              <ul>
+              <li>
+                <NavLink to='/'>Home</NavLink>
+
+              </li>
+              <li>
+                <NavLink to='/product'>Product</NavLink>
+
+              </li>
+              <li>
+                <NavLink to='/about'>About Us</NavLink>
+
+              </li>
+              <li>
+                <NavLink to='/contacts'>Contacts</NavLink>
+
+              </li>
+              </ul>
+
+            
+          </div>
+          <div className='authentication-container'>
+            <LuLogIn />
+            <LuLogOut />
+          </div>
+            
+          </div>
+         
+
+        </div>
+        
     </>
   );
 }
