232682210a55bcd58c028f5a648ab6696beb7923
Ochuodho <ochuodhoderrick@gmail.com>
2023-08-30 17:07:43 +0300

    modifying the navbar

--- groupproject/src/App.js
+++ groupproject/src/App.js
@@ -1,13 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
+
+    <>
+    <BrowserRouter>
+    <Navbar />
+    </BrowserRouter>
      
-    </div>
+     
+    </>
   );
 }
 
--- groupproject/src/components/Navbar.js
+++ groupproject/src/components/Navbar.js
@@ -1,27 +1,58 @@
+
+import { NavLink } from "react-router-dom"; 
+import { BsFillHouseHeartFill } from 'react-icons/bs'
+import { FaTruckMoving } from 'react-icons/fa';
+import { FaShoppingBag } from 'react-icons/fa'
+import { FaUser } from 'react-icons/fa'
+import './Navbar.css'
+
+
+
 function Navbar() {
   return (
-    <Nav>
-      <Logo>
-        <span>Shopify</span>
-      </Logo>
-      <LinksContainer>
-        <List>
-          <ListItem>
-            <NavLink to="/">Home</NavLink>
-          </ListItem>
-          <ListItem>
-            <NavLink to="/about">About</NavLink>
-          </ListItem>
-          <ListItem>
-            <NavLink to="/contact">Contact</NavLink>
-          </ListItem>
-          <ListItem>
-            <NavLink to="/login">Login</NavLink>
-          </ListItem>
-        </List>
-      </LinksContainer>
+    <>
+    <div className="free">
+     
+        <div className='icons'>
+        <FaTruckMoving /> 
+        </div>
+        <p>Free shipping upto Kshs 1000!!!  </p>
+      </div>
+      <div className='topheader'>
+        <div className='container'>
+          <div className='logo'>
+            <strong>SHOPify</strong>
+          </div>
+          <div className='search'>
+            <input type='text' value='' placeholder='search product'></input>
+            <button>Search</button>
+            <div className='icon'>
+              <div className='account'>
+                <div className='iconuser'>
+                <FaUser />
+                </div>
+                <p>Hello, user</p>
+              
+
+              </div>
+              <BsFillHouseHeartFill />
+              <FaShoppingBag />
+
+            </div>
+
+          </div>
+        </div>
+
+
+
+
+        </div>
+     
+    
       
-    </Nav>
+    
+  
+    </>
   );
 }
 
