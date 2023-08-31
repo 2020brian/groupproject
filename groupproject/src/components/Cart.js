18c007d08e9b3370a3ad07f9f20d533f60275e62
tobias <tobias.omondi@student.moringaschool.com>
2023-08-31 10:48:27 +0300

    cart on process

---
+++ groupproject/src/components/Cart.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import Homepage from './Homepage';
+
+const Cart = () => {
+  const [cartItems, setCartItems] = useState([]);
+
+  const removeFromCart = (productId) => {
+    const updatedCart = cartItems.filter(item => item.id !== productId);
+    setCartItems(updatedCart);
+  };
+
+  return (
+    <div>
+      <h2>Your Cart</h2>
+      <ul>
+        {cartItems.map(item => (
+          <li key={item.id}>
+            {item.name} - ${item.price}
+            <button onClick={() => removeFromCart(item.id)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Cart;
---
+++ groupproject/src/components/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h2>Welcome to Your Dashboard!</h2>
+      <p>This is the page where users can interact after logging in.</p>
+    </div>
+  );
+}
+
+export default Dashboard;
